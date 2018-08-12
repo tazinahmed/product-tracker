ebe10968d4c47e1cf661e986543ac199cd04d0cc
sakib-rahman-bangladesh <sakib.rahman.0000@gmail.com>
2018-08-13 00:24:06 +0600

    RELEASED v1.9.0: snackbar added by sunny

--- ci.sh
+++ ci.sh
@@ -1,4 +1,4 @@
-﻿#!/usr/bin/env bash
+#!/usr/bin/env bash
 ##################################################
 ### Script : ci.sh (14-10-2017)                ###
 ### CI     : Continious Integration            ###
@@ -25,8 +25,8 @@
 
 echo
 echo [ Done ]  Initializing who am I.
-git config --global user.email "sk375478771@gmail.com"
-git config --global user.name "Tanvir-Aunjum-Sunny"
+git config --global user.email "sakib.rahman.0000@gmail.com"
+git config --global user.name "sakib-rahman-bangladesh"
 
 
 ############
@@ -64,7 +64,7 @@ echo
 ## Solved issues
 ## Default: Commit skipped|forgotten
 
-git commit -m "Snackbar Added"
+git commit -m "RELEASED v1.9.0: snackbar added by sunny"
 
 ############
 # Step: 05 #
@@ -85,7 +85,7 @@ git remote -v
 echo
 echo [ Done ]  Updating remote URL.
 
-git remote set-url origin https://github.com/Tanvir-Aunjum-Sunny/product-tracker
+git remote set-url origin https://github.com/sakib-rahman-bangladesh/product-tracker
 
 ############
 # Step: 07 #
@@ -104,4 +104,4 @@ echo
 echo 08. Pushing local codebase to remote repo...Repo-to-repo collaboration: git push
 echo
 git push origin master
-git push --all -f https://github.com/Tanvir-Aunjum-Sunny/product-tracker
+git push --all -f https://github.com/sakib-rahman-bangladesh/product-tracker
