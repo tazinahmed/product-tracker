2f73dfa3f263cfd2ca986bac8bcf0611e357d247
sakib-rahman-bangladesh <sakib.rahman.0000@gmail.com>
2018-08-14 19:22:17 +0600

    Track changes of google-services.json

--- ci.sh
+++ ci.sh
@@ -64,7 +64,7 @@ echo
 ## Solved issues
 ## Default: Commit skipped|forgotten
 
-git commit -m "RELEASED v1.10.0, secure prod server added"
+git commit -m "Track changes of google-services.json"
 
 ############
 # Step: 05 #
